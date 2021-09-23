d1a3adb38e5839b3c185ad0c3ad7a2d1aaf4ff11
Vikashini Chandrasekaran - (Americas 2 - iDEAS-DX) <VI20068261@wipro.com>
2021-09-23 19:05:36 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function App() {
   return (
@@ -26,13 +27,18 @@ function App() {
         <Grid  xs={2}>
         <Typography variant="h5" className="header-message">Inbox</Typography>
         </Grid>
+        <Grid  xs={2}>
+        </Grid>
+        <Grid  xs={2}>
+        </Grid>
+        <Grid><HelpOutlineIcon></HelpOutlineIcon></Grid>
       </Grid>
       <Grid container  className="chatSection">
         <LeftSidePanel />
         <Conversation />
         <Chat />
         <User />
-        {/* <Test /> */}
+        <Test />
 
       </Grid>
     </div>
